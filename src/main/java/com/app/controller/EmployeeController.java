f08654bbe28c00759b6a4f8811cb4c8f5ae3c024
git commit -m initial <rajeevmarka55@gmail.com>
2024-09-29 01:49:21 +0530

    my 28 september commit

---
+++ src/main/java/com/app/controller/EmployeeController.java
@@ -0,0 +1,125 @@
+package com.app.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.entity.Employee;
+import com.app.service.IEmployeeService;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeController {
+
+	@Autowired
+	private IEmployeeService service;
+	
+	
+	@PostMapping("/save")
+	public ResponseEntity<String> saveEmp(@RequestBody Employee emp){
+		ResponseEntity<String> rsp=null;
+		try {
+		Integer id=service.saveEmp(emp);
+		String msg="emp saved with this '"+id+"' ";
+		rsp=new ResponseEntity<String>(msg, HttpStatus.CREATED);
+		}catch(Exception e) {
+			rsp=new ResponseEntity<String>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+			e.printStackTrace();
+		}
+		return rsp;
+		
+		
+	}
+	
+	
+	
+	
+	@GetMapping("/all")
+	public ResponseEntity<?> saveEmp(){
+		ResponseEntity<?> rsp=null;
+		List<Employee> list=service.getAllEmp();
+		if(list.isEmpty()||list==null) {
+			rsp=new ResponseEntity<String>("no list of emp found", HttpStatus.BAD_REQUEST);
+		}else {
+		    rsp=new ResponseEntity<List<Employee>>(list, HttpStatus.OK);
+	
+		}
+		return rsp;
+		
+		
+	}
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> deleteEmp(@PathVariable("id") Integer id){
+		boolean present=service.isPresent(id);
+		ResponseEntity<String> rsp=null;
+		if(present) {
+			service.deleteEmp(id);
+			rsp=new ResponseEntity<String>(" emp '"+id+"' deleted successfully",HttpStatus.OK);
+		}else {
+			rsp=new ResponseEntity<String>(" emp '"+id+"' not found",HttpStatus.BAD_REQUEST);
+		}
+		
+		return rsp;
+		
+	}
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<String> updateEmp(@PathVariable("id") Integer id,@RequestBody Employee emp){
+		boolean present=service.isPresent(id);
+		ResponseEntity<String> rsp=null;
+		
+			Optional<Employee> existingEmpOpt = service.getOneEmp(id);
+			if(existingEmpOpt.isPresent()) {
+			Employee existing = existingEmpOpt.get();
+			existing.setEmpId(existing.getEmpId());
+			existing.setEmpName(emp.getEmpName());
+			existing.setEmpAddress(emp.getEmpAddress());
+			service.updateEmp(existing);
+			rsp=new ResponseEntity<String>("emp '"+id+"'updated successffully",HttpStatus.OK);
+		    }else {
+			rsp=new ResponseEntity<String>("emp '"+id+"' not exist ",HttpStatus.BAD_REQUEST);
+		}
+		return rsp;
+		
+	}
+	
+	@GetMapping("/get/{id}")
+	public ResponseEntity<?> getEmpDetails(@PathVariable("id") Integer id){
+		boolean present=service.isPresent(id);
+		ResponseEntity<?> rsp=null;
+		if(present) {
+			Optional<Employee> oneempdetails=service.getOneEmp(id);
+			rsp=new ResponseEntity<Optional<Employee>>(oneempdetails,HttpStatus.OK);
+		}else {
+			rsp=new ResponseEntity<String>("emp not exist ",HttpStatus.BAD_REQUEST);
+		
+		
+	}
+	return rsp;
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
